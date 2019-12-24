ef93940210a1fe17706e830ed6c2bd39cea32cea
Pavel Petrov <pkpetrov@mail.ru>
2019-12-25 00:05:28 +0300

    Добавить загрузку настроек из папки, указанной в appsettings.json

--- NetCoreDemo.App/Program.cs
+++ NetCoreDemo.App/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -18,6 +19,16 @@ namespace HttpRequestProcessing
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                    var config = configBuilder.Build();
+                    var storagePath = config.GetValue<string>("FileSystemMessageBus:StorageFolderPath");
+                    if (!string.IsNullOrWhiteSpace(storagePath))
+                    {
+                        var filePath = Path.Combine(storagePath, "messageBusSettings.json");
+                        configBuilder.AddJsonFile(filePath);
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
